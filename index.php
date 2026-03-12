7e28075372b54cc314b2a5c08e21e5541e3c5f20
pampam1511 <puplampupamela@gmail.com>
2026-03-12 19:44:13 +0000

    created an if statement to check if the database is connected

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo "Hello World";
+require_once "config/database.php";
+
+$database = new Database();
+$conn = $database->getConnection();
+
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Anime watchlist </title>
+    </head>
+    <body>
+        <?php if($conn): ?>
+            <h1> Database connection successful! </h1>
+        <?php else: ?>
+            <h1> Database connection failed! </h1>
+        <?php endif; ?>
+    </body>
+</html>
