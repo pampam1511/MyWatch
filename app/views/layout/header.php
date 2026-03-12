950f0016977b68b2d81ab58def3cc310061a2a81
pampam1511 <puplampupamela@gmail.com>
2026-03-12 20:23:02 +0000

    create a header where it shows the list

---
+++ app/views/layout/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anime Tracker</title>
+    <link rel="stylesheet" href="/anime-tracker/public/css/style.css">
+</head>
+<body>
+
+<nav>
+    <a href="/anime-tracker">My List</a>
+    <a href="/anime-tracker/index.php?action=create">Add Anime</a>
+</nav>
