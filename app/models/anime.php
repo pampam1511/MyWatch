18fe3a9da687c63aa11b3aa71364c0b1321fba1e
pampam1511 <puplampupamela@gmail.com>
2026-03-12 20:15:11 +0000

    made sure that it shows all the anime and fixed merge conflict

---
+++ app/models/anime.php
@@ -0,0 +1,73 @@
+<?php
+class Anime {
+
+    private $conn;
+    private $table = "anime";
+
+    public $id;
+    public $title;
+    public $status;
+    public $rating;
+    public $current_episode;
+    public $total_episodes;
+    public $genre;
+    public $date_added;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function getAll() {
+        $sql = "SELECT * FROM " . $this->table;
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getOne($id) {
+        $sql = "SELECT * FROM " . $this->table . " WHERE id = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($data) {
+        $sql = "INSERT INTO " . $this->table . "
+                (title, status, rating, current_episode, total_episodes, genre, date_added)
+                VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute([
+            $data['title'],
+            $data['status'],
+            $data['rating'],
+            $data['current_episode'],
+            $data['total_episodes'],
+            $data['genre'],
+            date('Y-m-d')
+        ]);
+    }
+
+    public function update($id, $data) {
+        $sql = "UPDATE " . $this->table . "
+                SET title=?, status=?, rating=?,
+                current_episode=?, total_episodes=?, genre=?
+                WHERE id=?";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute([
+            $data['title'],
+            $data['status'],
+            $data['rating'],
+            $data['current_episode'],
+            $data['total_episodes'],
+            $data['genre'],
+            $id
+        ]);
+    }
+
+    public function delete($id) {
+        $sql = "DELETE FROM " . $this->table . " WHERE id = ?";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute([$id]);
+    }
+}
+?>
