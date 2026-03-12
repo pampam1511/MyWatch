2bd6d1e7757b18e100f46ef78c386503e1da8c4f
pampam1511 <puplampupamela@gmail.com>
2026-03-12 20:47:27 +0000

    had to change the route because it was giving errors and not showing the correct page

--- app/controllers/AnimeController.php
+++ app/controllers/AnimeController.php
@@ -1,58 +1,50 @@
 <?php
-require_once "config/database.php";
-require_once "models/anime.php";
+require_once __DIR__ . "/../../config/database.php";
+require_once __DIR__ . "/../../app/models/Anime.php";
 
-class AnimeController{
+class AnimeController {
+
     private $db;
     private $anime;
 
     public function __construct() {
         $database = new Database();
         $this->db = $database->getConnection();
-        $this->anime = new anime($this->db);
+        $this->anime = new Anime($this->db);
     }
 
-    // Show all anime
     public function index() {
         $animeList = $this->anime->getAll();
-        require_once "/../views/anime/index.php";
-
+        require_once __DIR__ . "/../views/anime/index.php";
     }
 
-    // Show one anime
     public function show($id) {
         $anime = $this->anime->getOne($id);
-        require_once "/../views/anime/show.php";
+        require_once __DIR__ . "/../views/anime/show.php";
     }
 
-    // Show add form
     public function create() {
-        require_once  "/../views/anime/create.php";
+        require_once __DIR__ . "/../views/anime/create.php";
     }
 
-    // Save new anime
     public function store($data) {
         $this->anime->create($data);
         header("Location: /anime-tracker");
     }
 
-    // Show edit form
     public function edit($id) {
         $anime = $this->anime->getOne($id);
-        require_once "/../views/anime/edit.php";
+        require_once __DIR__ . "/../views/anime/edit.php";
     }
 
-    // Save edited anime
     public function update($id, $data) {
         $this->anime->update($id, $data);
         header("Location: /anime-tracker");
     }
 
-    // Delete anime
     public function destroy($id) {
         $this->anime->delete($id);
         header("Location: /anime-tracker");
     }
 }
-
 ?>
